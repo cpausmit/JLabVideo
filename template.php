0598bc7b38bcc6c9e9abc05222ce7ce0a157f5c9
Christoph Paus <paus@mit.edu>
2015-03-20 20:11:15 -0400

    Web page styling.

--- template.php
+++ template.php
@@ -1,5 +1,6 @@
 <?php
 
+$debug = 0;
 $extension = ".ap4"; // unimportant default
 
 $fullFileTrunc = preg_replace("|.php$|","",$_SERVER['PHP_SELF']);
@@ -18,40 +19,41 @@ if (file_exists($fileTrunc . ".webm")) {
 }
 $file = $fileTrunc . $extension;
 
-// Get the base directory on our server
-
-//$dir = basename(dirname($_SERVER['PHP_SELF']));
-$dp = opendir(".");
-$pwd = getcwd();
-$countpath = ereg_replace("/","_",$pwd);
-$countpath = str_replace("/","_",$pwd);
-$countpath = "dir$countpath";
-
 // Write the html header
 
-echo "<!DOCTYPE html><html><head></head><body bgcolor=\"#CCCCCC\">\n";
+echo "<!DOCTYPE html><html><head></head>";
+echo "<style>";
+echo "a:link    {color:#000000; background-color:transparent; text-decoration:none}";
+echo "a:visited {color:#009000; background-color:transparent; text-decoration:none}";
+echo "a:hover   {color:#900000; background-color:transparent; text-decoration:underline}";
+echo "a:active  {color:#900000; background-color:transparent; text-decoration:underline}";
+echo "body.ex   {margin-top: 0px; margin-bottom: 25px; margin-right: 25px; margin-left: 25px;}";
+echo "</style>";
+echo "<body class=\"ex\" bgcolor=\"#EEEEEE\">\n";
 echo "<p style=\"font-family: arial;font-size: 28px;font-weight: bold;color:#900000;\">
-      <img src=\"mit-greywhite-footer3.gif\"> Junior Lab Orals Video</p>\n";
-
-// Debugging output
-//
-//echo "<p>fullFile Trunc = " . $fullFileTrunc;
-//echo "<br/>file Trunc   = " . $fileTrunc;
-//echo "<br/>file         = " . $file;
-//echo "<br/>countpath    = " . $countpath . "</p>\n";
+      <img src=\"mit-logo.png\" height=\"40\"> Video of a Junior Lab Oral</p>\n";
 
 // Core text for the file
 
-echo "<p>\n"
+echo "<p style=\"font-family: arial;font-size: 18px;font-weight: medium;color:#404040;\">\n";
 echo "Videos from this server are in formats that can be played with all normal video players.<br>\n";
 echo "Most browsers will play these videos. To stream the video and have your browser play it,<br>\n";
 echo "left-click the link below or right-click and download it.<br>\n";
 
 // Write the file link
 
-$ldash = strrpos($file,"-");
-$name = substr($file, 0, $ldash);
-echo "<h3>Link: <a href=$file>$name</a></h3>\n";
+echo "<p style=\"font-family: arial;font-size: 18px;font-weight:medium;color:#009000;\">\n";
+echo "<a href=$file>$file</a>\n";
+
+// Debugging output
+
+if ($debug>0) {
+  echo "<p style=\"font-family: arial;font-size: 18px;font-weight: medium;color:#007000;\">\n";
+  echo "== Debugging ==<br/>";
+  echo "<br/>fullFile Trunc = " . $fullFileTrunc;
+  echo "<br/>file Trunc     = " . $fileTrunc;
+  echo "<br/>file           = " . $file;
+}
 
 // Write the html footer
 
